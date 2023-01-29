62c365a64653b359cd5839dfa0c5a0a87ae3f238
ayushshah31 <shahayush934@gmail.com>
2023-01-29 14:36:23 +0530

    doctor pres page

--- lib/pages/prescription_dr.dart
+++ lib/pages/prescription_dr.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:async';
-import 'package:mnc_identifier_ocr/mnc_identifier_ocr.dart';
-import 'package:mnc_identifier_ocr/model/ocr_result_model.dart';
-
+import 'dart:io';
 
 class Pres extends StatefulWidget {
   const Pres({Key? key}) : super(key: key);
@@ -13,65 +11,159 @@ class Pres extends StatefulWidget {
 }
 
 class _PresState extends State<Pres> {
-  OcrResultModel? result;
+  bool textScanning = false;
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> scanKtp() async {
-    OcrResultModel? res;
-    try {
-      res = await MncIdentifierOcr.startCaptureKtp(
-          withFlash: true, cameraOnly: true);
-    } catch (e) {
-      debugPrint('something goes wrong $e');
-    }
+  XFile? imageFile;
 
-    if (!mounted) return;
-
-    setState(() {
-      result = res;
-    });
-  }
-
-  _imgGlr() async {
-    final XFile? image =
-    await ImagePicker().pickImage(source: ImageSource.gallery);
-    debugPrint('path: ${image?.path}');
-  }
-
-  _imgCmr() async {
-    final XFile? image =
-    await ImagePicker().pickImage(source: ImageSource.camera);
-    debugPrint('path: ${image?.path}');
-  }
+  String scannedText = "";
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Stack(
-          children: [
-            Text('Ktp data: ${result?.toJson()}'),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                      onPressed: scanKtp, child: const Text('PUSH HERE')),
-                  const SizedBox(height: 8),
-                  ElevatedButton(
-                      onPressed: _imgCmr, child: const Text('CAMERA')),
-                  const SizedBox(height: 8),
-                  ElevatedButton(
-                      onPressed: _imgGlr, child: const Text('GALLERY')),
-                ],
-              ),
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Text Recognition example"),
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
       ),
+      body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+                margin: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    if (textScanning) const CircularProgressIndicator(),
+                    if (!textScanning && imageFile == null)
+                      Container(
+                        width: 300,
+                        height: 300,
+                        color: Colors.grey[300]!,
+                      ),
+                    if (imageFile != null) Image.file(File(imageFile!.path)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 5),
+                            padding: const EdgeInsets.only(top: 10),
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.white,
+                                onPrimary: Colors.grey,
+                                shadowColor: Colors.grey[400],
+                                elevation: 10,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8.0)),
+                              ),
+                              onPressed: () {
+                                getImage(ImageSource.gallery);
+                              },
+                              child: Container(
+                                margin: const EdgeInsets.symmetric(
+                                    vertical: 5, horizontal: 5),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Icon(
+                                      Icons.image,
+                                      size: 30,
+                                    ),
+                                    Text(
+                                      "Gallery",
+                                      style: TextStyle(
+                                          fontSize: 13, color: Colors.grey[600]),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )),
+                        Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 5),
+                            padding: const EdgeInsets.only(top: 10),
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.white,
+                                onPrimary: Colors.grey,
+                                shadowColor: Colors.grey[400],
+                                elevation: 10,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8.0)),
+                              ),
+                              onPressed: () {
+                                getImage(ImageSource.camera);
+                              },
+                              child: Container(
+                                margin: const EdgeInsets.symmetric(
+                                    vertical: 5, horizontal: 5),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Icon(
+                                      Icons.camera_alt,
+                                      size: 30,
+                                    ),
+                                    Text(
+                                      "Camera",
+                                      style: TextStyle(
+                                          fontSize: 13, color: Colors.grey[600]),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      child: Text(
+                        scannedText,
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    )
+                  ],
+                )),
+          )),
     );
+  }
+
+  void getImage(ImageSource source) async {
+    try {
+      final pickedImage = await ImagePicker().pickImage(source: source);
+      if (pickedImage != null) {
+        textScanning = true;
+        imageFile = pickedImage;
+        setState(() {});
+        getRecognisedText(pickedImage);
+      }
+    } catch (e) {
+      textScanning = false;
+      imageFile = null;
+      scannedText = "Error occured while scanning";
+      setState(() {});
+    }
+  }
+
+  void getRecognisedText(XFile image) async {
+    final inputImage = InputImage.fromFilePath(image.path);
+    final textDetector = GoogleMlKit.vision.textRecognizer();
+    RecognizedText recognisedText = await textDetector.processImage(inputImage);
+    await textDetector.close();
+    scannedText = "";
+    for (TextBlock block in recognisedText.blocks) {
+      for (TextLine line in block.lines) {
+        scannedText = scannedText + line.text + "\n";
+      }
+    }
+    textScanning = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
   }
 }
